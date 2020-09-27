256480f83c2c19ecbece28bfedb078cb60024016
Artem Murenko <Murenko777@gmail.com>
2020-09-27 22:16:22 +0300

    remove some filters

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('main');
+    return view('home');
+});
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/editor', function () {
+    return view('editor');
 });
