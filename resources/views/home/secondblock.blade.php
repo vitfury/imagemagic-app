4d62bf52398b687533886cc50c1de17347531403
Artem Murenko <Murenko777@gmail.com>
2020-10-17 19:32:40 +0300

    Add new templates

---
+++ resources/views/home/secondblock.blade.php
@@ -0,0 +1,39 @@
+<section class="bg-light text-center py-10">
+                        <div class="container">
+                            <div class="row align-items-center justify-content-center">
+                                <div class="col-md-9 col-lg-6 order-1 order-lg-0" data-aos="fade-right">
+                                    <div class="content-skewed content-skewed-right"><img class="content-skewed-item img-fluid shadow-lg rounded-lg" src="img/Thirdblock-tablet-screenshot.png" /></div>
+                                </div>
+                                <div class="col-lg-6 order-0 order-lg-1 mb-5 mb-lg-0" data-aos="fade-left">
+                                    <div class="mb-5">
+                                        <h2>Here's What You Get</h2>
+                                        <p class="lead">When you purchase this UI Kit, you get access to a robust suite of powerful tools and components to help you build your next landing page quickly and easily.</p>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <h6>Landing Pages</h6>
+                                            <p class="mb-2 small">We've crafted landing page examples for many popular business and product types.</p>
+                                            <a class="small text-arrow-icon" href="#!">Learn More<i data-feather="arrow-right"></i></a>
+                                        </div>
+                                        <div class="col-md-6 mb-4">
+                                            <h6>Page Examples</h6>
+                                            <p class="mb-2 small mb-0">Use our pre-built page examples to quickly create inner pages to your website.</p>
+                                            <a class="small text-arrow-icon" href="#!">Learn More<i data-feather="arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <h6>Layouts</h6>
+                                            <p class="mb-2 small mb-0">Our flex box based layout options make your site beautifully responsive and adaptable to any device.</p>
+                                            <a class="small text-arrow-icon" href="#!">Learn More<i data-feather="arrow-right"></i></a>
+                                        </div>
+                                        <div class="col-md-6 mb-4">
+                                            <h6>Modular Sections</h6>
+                                            <p class="small mb-0">All of the sections on each page are modular, so you can drop them into an existing page, or start with a new one!</p>
+                                            <a class="small text-arrow-icon" href="#!">Learn More<i data-feather="arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
