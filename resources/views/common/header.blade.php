430922b38c432f46f5e9be8c03e9039ea9051f13
ih8bc <Murenko777@gmail.com>
2020-11-03 18:57:52 +0200

    change css path

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,28 +1,24 @@
 <nav class="navbar navbar-marketing navbar-expand-lg bg-white navbar-light navbar__heigth">
-                        <div class="container headerlogos">
-                            <a class="logostylesdesktop" href="index.html"><img class="logostylesdesktop" src="img/logo.png" alt="logo">
-                            <div class="logo-mobile">
-                            <a class="Logo-mobile-fox-item" href="index.html"><img class="logostylemobilefox" src="img/fox.png" alt="logo">
-                            <a class="" href="index.html"><img class="logostylemobilesign" src="img/sign.png" alt="logo"></a>
-                            </div>
-                            <button class="navbar-toggler navbar-toggler__border" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <div class="hamburger-menu">
-  <input id="menu__toggle" type="checkbox" />
-  <label class="menu__btn" for="menu__toggle">
-    <span></span>
-  </label>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav ml-auto mr-lg-5">
-                                    <li class="nav-item"><a class="nav-link" href="/">Home </a></li>
-                                    <li class="nav-item"><a class="nav-link" href="gallery">Sticker gallery </a></li>
-                                    <li class="nav-item"><a class="nav-link" href="contacts">Contacts </a></li>
-                                    <li class="nav-item"><a class="nav-link" href="index.html">About us </a></li>
-                                    <li class="nav-item"><a class="nav-link" href="index.html">FAQ </a></li>
+    <div class="container headerlogos">
+        <a class="logostylesdesktop" href="index.html"><img class="logostylesdesktop" src="img/logo.png" alt="logo"></a>
+        <div class="logo-mobile">
+            <a class="Logo-mobile-fox-item" href="/"><img class="logostylemobilefox" src="img/fox.png" alt="logo"></a>
+            <a class="" href="index.html"><img class="logostylemobilesign" src="img/sign.png" alt="logo"></a>
+        </div>
+        <a class="navbar-brand text-dark" href="index.html">SB UI Kit Pro</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i data-feather="menu"></i></button>
+                           
 
-                                </ul>
-                                <a class="btn-primary btn rounded-pill px-4 ml-lg-4" href="https://shop.startbootstrap.com/sb-ui-kit-pro">Log-in/sign-in<i class="fas fa-arrow-right ml-1"></i></a>
-                            </div>
-                        </div>
-                    </nav>
-                   
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            
+            <!-- <ul class="navbar-nav ml-auto mr-lg-5">
+                <li class="nav-item"><a class="nav-link" href="/">Home </a></li>
+                <li class="nav-item"><a class="nav-link" href="gallery">Sticker gallery </a></li>
+                <li class="nav-item"><a class="nav-link" href="contacts">Contacts </a></li>
+                <li class="nav-item"><a class="nav-link" href="index.html">About us </a></li>
+                <li class="nav-item"><a class="nav-link" href="index.html">FAQ </a></li>
+            </ul>
+            <a class="btn-primary btn rounded-pill px-4 ml-lg-4" href="https://shop.startbootstrap.com/sb-ui-kit-pro">Log-in/sign-in<i class="fas fa-arrow-right ml-1"></i></a> -->
+        </div>
+    </div>
+</nav>
+
