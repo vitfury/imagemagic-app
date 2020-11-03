9dfd241a22738d38fb6a5e1375d839dadd504de2
ih8bc <Murenko777@gmail.com>
2020-11-03 19:17:48 +0200

    fix header layout

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,24 +1,20 @@
-<nav class="navbar navbar-marketing navbar-expand-lg bg-white navbar-light navbar__heigth">
+<nav class="navbar navbar-marketing navbar-expand-lg bg-white navbar-light">
     <div class="container headerlogos">
         <a class="logostylesdesktop" href="index.html"><img class="logostylesdesktop" src="img/logo.png" alt="logo"></a>
         <div class="logo-mobile">
             <a class="Logo-mobile-fox-item" href="/"><img class="logostylemobilefox" src="img/fox.png" alt="logo"></a>
             <a class="" href="index.html"><img class="logostylemobilesign" src="img/sign.png" alt="logo"></a>
         </div>
-        <a class="navbar-brand text-dark" href="index.html">SB UI Kit Pro</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i data-feather="menu"></i></button>
-                           
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            
-            <!-- <ul class="navbar-nav ml-auto mr-lg-5">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i data-feather="menu"></i></button>
+        <div class="collapse navbar-collapse navbar-collapse__position" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mr-lg-5 text-center">
                 <li class="nav-item"><a class="nav-link" href="/">Home </a></li>
                 <li class="nav-item"><a class="nav-link" href="gallery">Sticker gallery </a></li>
                 <li class="nav-item"><a class="nav-link" href="contacts">Contacts </a></li>
                 <li class="nav-item"><a class="nav-link" href="index.html">About us </a></li>
                 <li class="nav-item"><a class="nav-link" href="index.html">FAQ </a></li>
             </ul>
-            <a class="btn-primary btn rounded-pill px-4 ml-lg-4" href="https://shop.startbootstrap.com/sb-ui-kit-pro">Log-in/sign-in<i class="fas fa-arrow-right ml-1"></i></a> -->
+            <a class="btn-teal btn rounded-pill px-4 ml-lg-4 navbar-loginbutton__center" href="https://shop.startbootstrap.com/sb-ui-kit-pro">Login<i class="fas fa-arrow-right ml-1"></i></a>
         </div>
     </div>
 </nav>
-
