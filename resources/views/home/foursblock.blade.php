362bb56f47129785242a557ffa81578dc474d977
Omelchenko <webmaster@email.ua>
2020-11-03 20:49:43 +0200

    Add texts for main page + fixes

--- resources/views/home/foursblock.blade.php
+++ resources/views/home/foursblock.blade.php
@@ -1,18 +1,95 @@
-<section class="bg-light py-5 foursblock-main-page">
-                        <div class="container">
-                          
-                          
-                            <div class="card mb-4">
-                                <div class="card-body p-0">
-                                    <div class="row no-gutters">
-                                        <div class="col-lg-6 p-5">
-                                            <a class="badge badge-marketing badge-primary-soft badge-pill text-primary mb-3" href="#!">Web</a><a class="text-dark" href="#!"><h1>News companies post headline about creating new websites with powerful UI kits</h1></a>
-                                            <p>When creating and developing startups, using a UI kit to bootstrap your companies website can be an extremely efficient way to save time, money, and runway.</p>
-                                            <a class="text-arrow-icon small" href="#!">Read more<i data-feather="arrow-right"></i></a>
-                                        </div>
-                                        <div class="col-lg-6 align-self-stretch bg-img-cover d-none d-lg-flex" style='background-image: url("https://source.unsplash.com/npxXWgQ33ZQ/1200x800")'></div>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                    </section>
+<section class="bg-light pb-15 pt-10 foursblock-main-page">
+    <div class="container">
+
+
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="text-center">
+                    <div class="text-xs text-uppercase-expanded text-primary mb-2">Application Features</div>
+                    <h2 class="mb-5">We have a simple solution to a complex problem</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5" data-aos="fade-up">
+                <a class="card text-center text-decoration-none h-100 lift" href="#!"
+                ><div class="card-body py-5">
+                        <div class="icon-stack icon-stack-lg bg-green-soft text-green mb-4"><i data-feather="layers"></i></div>
+                        <h5>Components</h5>
+                        <p class="card-text small">Our product is full of customized and extendable components.</p>
+                    </div></a
+                >
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5" data-aos="fade-up" data-aos-delay="100">
+                <a class="card text-center text-decoration-none h-100 lift" href="#!"
+                ><div class="card-body py-5">
+                        <div class="icon-stack icon-stack-lg bg-red-soft text-red mb-4"><i data-feather="tool"></i></div>
+                        <h5>Utilities</h5>
+                        <p class="card-text small">Powerful utility classes makes it easy to customize your product.</p>
+                    </div></a
+                >
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5" data-aos="fade-up" data-aos-delay="150">
+                <a class="card text-center text-decoration-none h-100 lift" href="#!"
+                ><div class="card-body py-5">
+                        <div class="icon-stack icon-stack-lg bg-yellow-soft text-yellow mb-4"><i data-feather="layout"></i></div>
+                        <h5>Layouts</h5>
+                        <p class="card-text small">Our flex based layouts offer a beautifully reponsive approach.</p>
+                    </div></a
+                >
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 mb-lg-0" data-aos="fade-up">
+                <a class="card text-center text-decoration-none h-100 lift" href="#!"
+                ><div class="card-body py-5">
+                        <div class="icon-stack icon-stack-lg bg-purple-soft text-purple mb-4"><i data-feather="book"></i></div>
+                        <h5>Documentation</h5>
+                        <p class="card-text small">Our code is fully documented with instructions and usage examples.</p>
+                    </div></a
+                >
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 mb-md-0" data-aos="fade-up" data-aos-delay="100">
+                <a class="card text-center text-decoration-none h-100 lift" href="#!"
+                ><div class="card-body py-5">
+                        <div class="icon-stack icon-stack-lg bg-blue-soft text-blue mb-4"><i data-feather="code"></i></div>
+                        <h5>Code Snippets</h5>
+                        <p class="card-text small">Use code snippets to easily drop new components into your project.</p>
+                    </div></a
+                >
+            </div>
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="150">
+                <a class="card text-center text-decoration-none h-100 lift" href="#!"
+                ><div class="card-body py-5">
+                        <div class="icon-stack icon-stack-lg bg-orange-soft text-orange mb-4"><i data-feather="package"></i></div>
+                        <h5>Plugins</h5>
+                        <p class="card-text small">We've implemented powerful third party plugins for added functionality.</p>
+                    </div></a
+                >
+            </div>
+        </div>
+
+        <div class="svg-border-waves text-dark">
+            <svg class="wave" style="pointer-events: none" fill="currentColor" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 75">
+                <defs>
+                    <style>
+                        .a {
+                            fill: none;
+                        }
+                        .b {
+                            clip-path: url(#a);
+                        }
+                        .d {
+                            opacity: 0.5;
+                            isolation: isolate;
+                        }
+                    </style>
+                    <clipPath id="a"><rect class="a" width="1920" height="75"></rect></clipPath>
+                </defs>
+                <title>wave</title>
+                <g class="b"><path class="c" d="M1963,327H-105V65A2647.49,2647.49,0,0,1,431,19c217.7,3.5,239.6,30.8,470,36,297.3,6.7,367.5-36.2,642-28a2511.41,2511.41,0,0,1,420,48"></path></g>
+                <g class="b"><path class="d" d="M-127,404H1963V44c-140.1-28-343.3-46.7-566,22-75.5,23.3-118.5,45.9-162,64-48.6,20.2-404.7,128-784,0C355.2,97.7,341.6,78.3,235,50,86.6,10.6-41.8,6.9-127,10"></path></g>
+                <g class="b"><path class="d" d="M1979,462-155,446V106C251.8,20.2,576.6,15.9,805,30c167.4,10.3,322.3,32.9,680,56,207,13.4,378,20.3,494,24"></path></g>
+                <g class="b"><path class="d" d="M1998,484H-243V100c445.8,26.8,794.2-4.1,1035-39,141-20.4,231.1-40.1,378-45,349.6-11.6,636.7,73.8,828,150"></path></g>
+            </svg>
+        </div>
+
+</section>
--- resources/views/home/secondblock.blade.php
+++ resources/views/home/secondblock.blade.php
@@ -1,39 +1,32 @@
-<section class="bg-light text-center py-10">
-                        <div class="container">
-                            <div class="row align-items-center justify-content-center">
-                                <div class="col-md-9 col-lg-6 order-1 order-lg-0" data-aos="fade-right">
-                                    <div class="content-skewed content-skewed-right"><img class="content-skewed-item img-fluid shadow-lg rounded-lg" src="img/Thirdblock-tablet-screenshot.png" /></div>
-                                </div>
-                                <div class="col-lg-6 order-0 order-lg-1 mb-5 mb-lg-0" data-aos="fade-left">
-                                    <div class="mb-5">
-                                        <h2>Here's What You Get</h2>
-                                        <p class="lead">When you purchase this UI Kit, you get access to a robust suite of powerful tools and components to help you build your next landing page quickly and easily.</p>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6 mb-4">
-                                            <h6>Landing Pages</h6>
-                                            <p class="mb-2 small">We've crafted landing page examples for many popular business and product types.</p>
-                                            <a class="small text-arrow-icon" href="#!">Learn More<i data-feather="arrow-right"></i></a>
-                                        </div>
-                                        <div class="col-md-6 mb-4">
-                                            <h6>Page Examples</h6>
-                                            <p class="mb-2 small mb-0">Use our pre-built page examples to quickly create inner pages to your website.</p>
-                                            <a class="small text-arrow-icon" href="#!">Learn More<i data-feather="arrow-right"></i></a>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6 mb-4">
-                                            <h6>Layouts</h6>
-                                            <p class="mb-2 small mb-0">Our flex box based layout options make your site beautifully responsive and adaptable to any device.</p>
-                                            <a class="small text-arrow-icon" href="#!">Learn More<i data-feather="arrow-right"></i></a>
-                                        </div>
-                                        <div class="col-md-6 mb-4">
-                                            <h6>Modular Sections</h6>
-                                            <p class="small mb-0">All of the sections on each page are modular, so you can drop them into an existing page, or start with a new one!</p>
-                                            <a class="small text-arrow-icon" href="#!">Learn More<i data-feather="arrow-right"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+<section class="bg-light text-center py-15">
+    <div class="container mt-5">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-lg-6 order-0 order-lg-1 mb-5 mb-lg-0 text-left" data-aos="fade-left">
+                <div class="mb-5">
+                    <h2>How it works</h2>
+                    <p class="lead">You don't need to install compicated software and read tons of documentation. Simply open the editor, upload your photo, remove background, add a caption, and you are ready to go</p>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <h6>Remove background</h6>
+                        <p class="mb-2 small">We use Artificial intelligence built on best known algorythms, to remove background with the highest accuracy</p>
+                        <a class="small text-arrow-icon" href="/editor">Try now<i data-feather="arrow-right"></i></a>
+                    </div>
+                    <div class="col-md-6 mb-4">
+                        <h6>WhatsApp and Telegram</h6>
+                        <p class="mb-2 small mb-0">Follow our simple guide to publish your newly created sticker into your favorite messenger</p>
+                        <a class="small text-arrow-icon" href="/faq#publish">Read guide<i data-feather="arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-9 col-lg-6 order-1 order-lg-0" data-aos="fade-right">
+                <div class="device-laptop text-gray-200 aos-init aos-animate" data-aos="fade-up">
+                    <svg class="device-container" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="83.911 298.53 426.962 243.838"><path d="M474.843 516.208V309.886c0-6.418-4.938-11.355-11.354-11.355H131.791c-6.417 0-11.354 4.938-11.354 11.355v206.816H83.911v13.326c4.938 7.896 31.098 12.34 40.969 12.34h345.024c10.366 0 36.526-4.936 40.969-12.34v-13.326h-36.03v-.494zM134.26 313.341h326.762v203.361H134.26V313.341z"></path></svg><img class="device-screenshot" src="assets/img/app-desktop-demo-screenshot.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="svg-border-rounded text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
+    </div>
+</section>
