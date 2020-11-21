58bdf16382a96284fd98fb016a287dd4456d7a62
Omelchenko <webmaster@email.ua>
2020-11-21 15:58:04 +0200

    Add stickers table && storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,11 @@ return [
             'visibility' => 'public',
         ],
 
+        'volume' => [
+            'driver' => 'local',
+            'root' => env('STORAGE_VOLUME_PATH'),
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
---
+++ database/migrations/2020_11_19_201110_create_stickers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStickersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stickers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('name', 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stickers');
+    }
+}
